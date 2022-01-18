8450be2cb911db133125f76775ad2435c2b46d7d
Pavel07-chylden <pates1993@mail.ru>
2022-01-18 19:24:31 +0300

    init new data

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,6 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom 
+// expect(element).toHaveTextContent(/react/i)
+
 import '@testing-library/jest-dom';
