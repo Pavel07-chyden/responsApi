c80cfd52f8ee311f1e204dcadc0e3765a9b42d56
Pavel07-chylden <pates1993@mail.ru>
2022-01-20 23:44:24 +0300

    update

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,4 +3,26 @@
 // learn more: https://github.com/testing-library/jest-dom 
 // expect(element).toHaveTextContent(/react/i)
 
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom 
+// expect(element).toHaveTextContent(/react/i)
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom 
+// expect(element).toHaveTextContent(/react/i)
+
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom 
+// expect(element).toHaveTextContent(/react/i)
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom 
+// expect(element).toHaveTextContent(/react/i)
+
 import '@testing-library/jest-dom';
